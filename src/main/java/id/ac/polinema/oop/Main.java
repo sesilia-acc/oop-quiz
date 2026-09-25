254990cfdf054abbb80bdaa50c78e5cc3884f4fe
sesilia-acc <dandinsesilia@gmail.com>
2026-09-25 09:09:25 +0700

    update main

--- src/main/java/id/ac/polinema/oop/Main.java
+++ src/main/java/id/ac/polinema/oop/Main.java
@@ -12,7 +12,24 @@ package id.ac.polinema.oop;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Complete the classes first, then write your demo here.");
-        System.out.println("See the 'Try the App Manually' section in README.md.");
+        Menu menu = new Menu(10);
+        menu.addMenuItem(new MenuItem("Nasi Goreng", 15000));
+        menu.addMenuItem(new MenuItem("Ayam Geprek", 10000));
+        menu.addMenuItem(new MenuItem("Mie Ayam", 12000));
+        menu.addMenuItem(new MenuItem("Pop Ice", 5000));
+        
+        Customer customer = new Customer("C001", "Joko Kendil");
+        Order order = new Order(customer);
+        order.addItem(menu.findItem("Mie Ayam"), 3);
+        order.addItem(menu.findItem("Ayam Geprek"), 10);
+
+        Cashier cashier = new Cashier();
+        double cash = 200000;
+
+        System.out.println("Customer    : " + order.getCustomer().getName());
+        System.out.println("Total       : " + order.getTotal());
+        System.out.println("Payable     : " + order.getFinalTotal());
+        System.out.println("Cash        : " + cash);
+        System.out.println("Change      : " + cashier.calculateChange(order, cash));
     }
 }
