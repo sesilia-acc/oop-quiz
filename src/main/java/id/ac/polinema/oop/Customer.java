c1e6cffe365a91d88ff21094118e03f98559af07
sesilia-acc <dandinsesilia@gmail.com>
2026-09-25 08:34:54 +0700

    update customer

--- src/main/java/id/ac/polinema/oop/Customer.java
+++ src/main/java/id/ac/polinema/oop/Customer.java
@@ -11,6 +11,8 @@ package id.ac.polinema.oop;
  * (all fields must be private).
  */
 public class Customer {
+    private String customerId;
+    private String name;
 
     /**
      * Creates a customer and stores both parameters into the fields.
@@ -19,15 +21,16 @@ public class Customer {
      * @param name       customer's name
      */
     public Customer(String customerId, String name) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.customerId = customerId;
+        this.name = name;
     }
 
     public String getCustomerId() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return customerId;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return name;
     }
 
     /**
@@ -37,6 +40,8 @@ public class Customer {
      * @param name new name
      */
     public void setName(String name) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (name != null) {
+            this.name = name;
+        }
     }
 }
